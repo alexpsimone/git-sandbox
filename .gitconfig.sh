0d5b2b604e2d7d37442ccb1a4ab94b14b8ab2725
Simone Alexandra (BEG/MSD-CNA1) <alexandra.simone@us.bosch.com>
2023-01-12 15:44:01 -0800

    added line to shell script

--- .gitconfig.sh
+++ .gitconfig.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 git config --local include.path './.gitconfig'
--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "clean.sh was called."
 export SHA="This is a cat meow meow meow."
 sed -i '/file/a/\'"$SHA" test.txt
